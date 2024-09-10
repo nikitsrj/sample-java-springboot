896c76a95625f8b8d38c4bdbf1fa64b32b5306cb
root <root@ip-172-31-82-49.ec2.internal>
2024-09-10 12:40:09 +0000

    Initial Push

---
+++ src/main/java/com/example/helloworldwebapp/HelloWorldWebApp.java
@@ -0,0 +1,22 @@
+package com.example.helloworldwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class HelloWorldWebApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldWebApp.class, args);
+    }
+
+    @RestController
+    class HelloWorldController {
+        @GetMapping("/")
+        public String helloWorld() {
+            return "Hello World!";
+        }
+    }
+}
